3fd5b3a4a577c3a0532c3430e8b9c03566e2cbbd
Carlos Casa <carlos.cupeiro@grupocmc.es>
2017-01-21 20:20:33 +0100

    Refactor inicio

---
+++ app/src/main/java/com/upvmaster/carlos/audiolibros/adapters/SearchObservable.java
@@ -0,0 +1,12 @@
+package com.upvmaster.carlos.audiolibros.adapters;
+
+import java.util.Observable;
+
+import android.support.v7.widget.SearchView;
+
+/**
+ * Created by Carlos on 21/01/2017.
+ */
+
+public class SearchObservable extends Observable implements SearchView.OnQueryTextListener {
+}
---
+++ app/src/main/java/com/upvmaster/carlos/audiolibros/entities/LibroStorage.java
@@ -0,0 +1,8 @@
+package com.upvmaster.carlos.audiolibros.entities;
+
+/**
+ * Created by Carlos on 21/01/2017.
+ */
+
+public class LibroStorage {
+}
